68ba12d08b9bb98af9608924d4a6746d6bb48b0f
Pliterature <1051209415@qq.com>
2018-10-18 18:10:14 +0800

    create

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,12 @@ Page({
   data: {
     show: false,
     back: false,
-    textareac : false
+    textareac : false,
+    hour: 0,
+    minute : 0,
+    second : 0,
+    millisecond : 0,
+    int : null
   },
   onLoad: function () {
 
@@ -13,11 +18,46 @@ Page({
       back: true,
       textareac : true
     });
+    this.starttimer();
   },
   End:function(){
     this.setData({
       show: false,
       textareac : false
     });
+  },
+  starttimer(){
+    var self = this;
+    this.setData({
+      int : setInterval(self.timer(), 50)
+    })
+  },
+  timer:function(){
+    var mil = this.data.millisecond;
+    this.setData({
+      millisecond : mil + 50
+    })
+    if (this.data.millisecond >= 1000){
+      var sec = this.data.second;
+      this.setData({
+        millisecond: 0,
+        second : sec + 1
+      })
+    }
+    if (this.data.second >= 60) {
+      var min = this.data.minute;
+      this.setData({
+        minute: min + 1,
+        second : 0
+      })
+    }
+
+    if (this.data.minute >= 60) {
+      var hou = this.data.hour;
+      this.setData({
+        minute: 0,
+        hour: hou +1
+      })
+    }
   }
 })
